fc8900546209040e745a52a974f6a36be4124809
Juan Rodriguez <juangasca95@gmail.com>
2022-10-13 22:41:04 -0500

    replacing console.log for Logger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@/modules/config/config.service';
 
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common/services';
+
+const logger = new Logger('main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  await app.listen(configService.port);
+  await app.listen(configService.port, () =>
+    logger.log(`Server is running on port ${configService.port}`)
+  );
 }
 bootstrap();
