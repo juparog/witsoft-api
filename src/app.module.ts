5c95e771671905622a6814298d28a009fbc7d225
Juan Rodriguez <juangasca95@gmail.com>
2022-10-14 10:23:22 -0500

    configuration with @nestjs/config, removing convict package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
-
-import { ConfigModule } from '@/modules/config/config.module';
-import { ConfigService } from '@/modules/config/config.service';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.NODE_ENV || 'development'}`, '.env']
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService, ConfigService]
+  providers: [AppService]
 })
 export class AppModule {}
